f5f70ce1773267e3f6a834d37ab4775e9308a5bd
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-06-03 20:46:21 -0600

    getVideo2

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.get('/products/:id', (req, res) => {
   })
 });
 
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json(
+    {
+      categoryId,
+      productId
+  })
+});
 
 
 app.listen(port, () => {
