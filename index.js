e78302dbbbdd9ccda774e71f96d2e8a8af2a6523
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-06-04 01:38:23 -0600

    getVideo4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -8,40 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hola mi server')
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url()
-    })
+routerApi(app)
 
-  }
-
-  res.json(products);
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json(
-    {
-    id,
-    name: 'Product',
-    price: 1000
-  })
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json(
-    {
-      categoryId,
-      productId
-  })
-});
+// app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//   const { categoryId, productId } = req.params;
+//   res.json(
+//     {
+//       categoryId,
+//       productId
+//   })
+// });
 
 
 app.listen(port, () => {
