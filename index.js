360ccd7512b7e614703a2f4cf11fb2718be6ac03
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-06-04 00:55:31 -0600

    getVideo3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 
 const app = express();
 const port = 3000;
@@ -8,12 +9,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-    name: 'Product',
-    price: 1000
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url()
+    })
+
   }
-])
+
+  res.json(products);
 });
 
 app.get('/products/:id', (req, res) => {
