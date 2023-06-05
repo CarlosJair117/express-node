1908f8405de40c49ca0f89064d620fcb3115c579
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-06-05 02:25:26 -0600

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {logErrors, errorHandler} = require('./middlewares/error.handler')
@@ -6,7 +7,20 @@ const {logErrors, errorHandler} = require('./middlewares/error.handler')
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://myapp.co']
+const options = {
+  origin: (origin, callback) => {
+    if( whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server')
