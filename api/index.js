f2a310ea0d35bf151bf3aecd25d79ef70c669f52
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-06-05 02:55:32 -0600

    vercel

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const routerApi = require('./routes');
 const {logErrors, errorHandler} = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // app.use(cors(options));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en express')
 });
 
