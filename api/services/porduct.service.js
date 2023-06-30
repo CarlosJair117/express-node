de15723d8a169c16307e64089213f724fa2a0c6e
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2023-06-29 23:32:24 -0600

    filtrado

--- api/services/porduct.service.js
+++ api/services/porduct.service.js
@@ -2,6 +2,7 @@ const { faker } = require('@faker-js/faker');
 const { models } = require('../../libs/sequelize')
 const boom = require('@hapi/boom');
 const { options } = require('joi');
+const { Op } = require('sequelize');
 
 class ProductsService {
 
@@ -31,11 +32,19 @@ class ProductsService {
   async find(query) {
     const options = {
       include: ['category'],
+      where: {}
     }
     const {limit, offset} = query;
     if (limit, offset){
       options.limit = limit;
       options.offset = offset;
+    }
+    const { price_min, price_max } = query;
+    if(price_min && price_max){
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max
+      };
     }
     const products = await models.Product.findAll(options)
     return products;
